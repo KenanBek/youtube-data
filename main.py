e04921d2e2982332b490eddf857bd8a516fc2847
Kenan Bek <mail@kenanbek.me>
2018-02-26 06:13:40 +0200

    Add basic demo functions

--- main.py
+++ main.py
@@ -1,74 +1,55 @@
-# YouTube Data
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-Example application to demonstrate some YouTube Data APIv3 features.
+import argparse
+import iso3166
+from youtube import YouTubeAPI
 
-### Setup
 
-    git clone https://github.com/KenanBek/python-data
-    cd python-data
-    virtualenv env
-    source env/bin/activate (or .\env\Scripts\activate)
-    pip install -r requirements.txt
+def enc(text):
+    """
+    Return correctly encoded string.
+    
+    :param text: 
+    :return: 
+    """
+    if isinstance(text, str):
+        return unicode(text, 'utf-8')  # TODO: fix in Python 3
+    elif isinstance(text, unicode):
+        return text.encode('utf-8')
+    else:
+        raise Exception("Unsupported encode format.")
 
-### Usage
 
-To see all demo commands use
-
-    python main.py demo -h
-
-All communication goes through (python-youtube-apiv3)[https://github.com/KenanBek/python-youtube-apiv3].
-It is basic wrapper around Google's API. Here we use it just by creating new instance of the API with API key.
-
-    api = YouTubeAPI('API-KEY')
-
-Example of search function:
-
-    def search_list(self, **kwargs):
-        """
-        Returns a collection of search results that match the query parameters specified in the API request. 
-        By default, a search result set identifies matching video, channel, and playlist resources, 
-        but you can also configure queries to only retrieve a specific type of resource.
-
-        More details: https://developers.google.com/youtube/v3/docs/search/list
-
-        :param kwargs: 
-        :return: 
-        """
-        kwargs = remove_empty_kwargs(**kwargs)
-
-        results = self.service.search().list(
-            **kwargs
-        ).execute()
-
-        return results
-
-### Demo Commands
-
-#### List YouTube categories for given country list
-
-    python main.py demo -c
-
-Code:
+def demo_categories(api):
+    # List categories by country
 
     countries = [
         iso3166.countries.get('us'),
         iso3166.countries.get('de'),
     ]
+
+    # We can use simple list instead of iso3166, but with ISO module we have all countries with details
+    # If you want to uncomment this then you need to modify code bellow to access dict items:
+    # so, instead of country.alpha2 put country['alpha2']
+    #
+    # countries = [
+    #     {'name': 'United States', 'alpha2': 'US'},
+    #     {'name': 'Germany', 'alpha2': 'DE'},
+    # ]
+
     for country in countries:
         categories = api.video_categories_list(
             part='snippet',
             regionCode=country.alpha2
         )
+
         print("\n\tCategories for {}".format(country.name))
         for category in categories['items']:
             print(category['snippet']['title'])
 
-#### Show basic info and stats for given channels
 
-    python main.py demo -ch
-
-Code:
-
+def demo_channels(api):
     channels = [
         'selgomez',
         'JustinBieberVEVO',
@@ -91,12 +72,8 @@ Code:
         print("\tComments: {}".format(channel_info['items'][0]['statistics']['commentCount']))
         print('')
 
-#### Show most recent videos of given channels
 
-    python main.py demo -cv
-
-Code:
-
+def demo_channel_videos(api):
     channels = [
         'selgomez',
         'JustinBieberVEVO',
@@ -128,12 +105,8 @@ Code:
             print(video_desc)
         print('')
 
-#### Show most popular videos in the Germany
 
-    python main.py demo -tv
-
-Code:
-
+def demo_trending_videos(api):
     popular_videos = api.videos_list(
         part='snippet',
         chart='mostPopular',
@@ -144,12 +117,8 @@ Code:
         video_title = enc(popular_video['snippet']['title'])
         print(video_title)
 
-#### Search for given term and order results by rating in the Germany
 
-    python main.py demo -q python
-
-Code:
-
+def demo_search(api, query):
     popular_videos = api.search_list(
         part='snippet',
         q=query,
@@ -160,3 +129,62 @@ Code:
     for popular_video in popular_videos['items']:
         video_title = enc(popular_video['snippet']['title'])
         print(video_title)
+
+
+def demo(command):
+    api = YouTubeAPI('AIzaSyBGvRcUjTaMQi9ry585d09nWdbed7rRdBs')
+    if command.categories:
+        demo_categories(api)
+    if command.channels:
+        demo_channels(api)
+    if command.channel_videos:
+        demo_channel_videos(api)
+    if command.trending_videos:
+        demo_trending_videos(api)
+    if command.query:
+        demo_search(api, command.query)
+
+
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser()
+    argument_subparsers = argument_parser.add_subparsers(help="use following sub-commands")
+
+    command_demo = argument_subparsers.add_parser("demo", help='demo commands')
+    command_demo.add_argument(
+        '-c',
+        '--categories',
+        dest="categories",
+        action="store_true",
+        help="show categories in the given regions"
+    )
+    command_demo.add_argument(
+        '-ch',
+        '--channels',
+        dest="channels",
+        action="store_true",
+        help="show channel basic statistics"
+    )
+    command_demo.add_argument(
+        '-cv',
+        '--ch-videos',
+        dest="channel_videos",
+        action="store_true",
+        help="show channel's most recent videos"
+    )
+    command_demo.add_argument(
+        '-tv',
+        '--tr-videos',
+        dest="trending_videos",
+        action="store_true",
+        help="show trending videos in germany"
+    )
+    command_demo.add_argument(
+        '-q',
+        '--query',
+        dest="query",
+        help="search and order by rating (in DE)"
+    )
+    command_demo.set_defaults(func=demo)
+
+    args = argument_parser.parse_args()
+    args.func(args)
--- youtube.py
+++ youtube.py
@@ -1,9 +1,102 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from googleapiclient.discovery import build
 
 
+# Remove keyword arguments that are not set
+def remove_empty_kwargs(**kwargs):
+    good_kwargs = {}
+    if kwargs is not None:
+        for key, value in kwargs.iteritems():
+            if value:
+                good_kwargs[key] = value
+    return good_kwargs
+
+
 class YouTubeAPI(object):
+    """
+    Wrapper class around Google's YouTube API for Python.
+    
+    For more information about YouTube Data APIv3 visit:
+    https://developers.google.com/youtube/v3/
+    
+    This code also available as a separate library:
+    https://github.com/KenanBek/python-youtube-apiv3
+    
+    """
     API_SERVICE_NAME = 'youtube'
     API_VERSION = 'v3'
 
     def __init__(self, api_key):
-        self.build_obj = build(YouTubeAPI.API_SERVICE_NAME, YouTubeAPI.API_VERSION, developerKey=api_key)
+        self.service = build(YouTubeAPI.API_SERVICE_NAME, YouTubeAPI.API_VERSION, developerKey=api_key)
+
+    def channels_list(self, **kwargs):
+        """
+        Returns a collection of zero or more channel resources that match the request criteria.
+        
+        More details: https://developers.google.com/youtube/v3/docs/channels/list
+        
+        :param kwargs: 
+        :return: 
+        """
+        kwargs = remove_empty_kwargs(**kwargs)
+
+        results = self.service.channels().list(
+            **kwargs
+        ).execute()
+
+        return results
+
+    def video_categories_list(self, **kwargs):
+        """
+        Returns a list of categories that can be associated with YouTube videos.
+        
+        More details: https://developers.google.com/youtube/v3/docs/videoCategories/list
+        
+        :param kwargs: 
+        :return: 
+        """
+        kwargs = remove_empty_kwargs(**kwargs)
+
+        results = self.service.videoCategories().list(
+            **kwargs
+        ).execute()
+
+        return results
+
+    def videos_list(self, **kwargs):
+        """
+        Returns a list of videos that match the API request parameters.
+
+        More details: https://developers.google.com/youtube/v3/docs/videos/list
+
+        :param kwargs: 
+        :return: 
+        """
+        kwargs = remove_empty_kwargs(**kwargs)
+
+        results = self.service.videos().list(
+            **kwargs
+        ).execute()
+
+        return results
+
+    def search_list(self, **kwargs):
+        """
+        Returns a collection of search results that match the query parameters specified in the API request. 
+        By default, a search result set identifies matching video, channel, and playlist resources, 
+        but you can also configure queries to only retrieve a specific type of resource.
+
+        More details: https://developers.google.com/youtube/v3/docs/search/list
+
+        :param kwargs: 
+        :return: 
+        """
+        kwargs = remove_empty_kwargs(**kwargs)
+
+        results = self.service.search().list(
+            **kwargs
+        ).execute()
+
+        return results
